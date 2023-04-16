cba13787180bd034c34a94fd72e2bee47955eeb5
Luis Hernandez <lhdez@iuma.ulpgc.es>
2023-04-16 13:32:22 +0100

    initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,74 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+      declarations: [ AppComponent ],
+      imports: [ FormsModule ]
+    })
+      .compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'shopping-list'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('shopping-list');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shopping-list app is running!');
   });
+
+
+  test('should add a new item to the list', () => {
+    const itemName = 'Leche';
+    const itemInput = fixture.nativeElement.querySelector('input');
+    itemInput.value = itemName;
+    itemInput.dispatchEvent(new Event('input'));
+
+    const addButton =
+      fixture.nativeElement.querySelector('button[type="submit"]');
+    addButton.click();
+
+    fixture.detectChanges();
+
+    expect(component.items.length).toBe(1);
+    expect(component.items[0].name).toBe(itemName);
+  });
+
+  test('should complete an item in the list', () => {
+    const itemName = 'Leche';
+    const newItem = { name: itemName, completed: false };
+    component.items.push(newItem);
+
+    fixture.detectChanges();
+
+
+    const completeButton =
+      fixture.nativeElement.querySelector('button[id="complete"]');
+    completeButton.click();
+
+    fixture.detectChanges();
+
+    expect(component.items[0].completed).toBe(true);
+  });
+
+  test('should delete an item from the list', () => {
+    const itemName = 'Leche';
+    const newItem = { name: itemName, completed: false };
+    component.items.push(newItem);
+
+    fixture.detectChanges();
+
+    const deleteButton =
+      fixture.nativeElement.querySelector('button[id="delete"]');
+    deleteButton.click();
+
+    fixture.detectChanges();
+
+    expect(component.items.length).toBe(0);
+  });
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  name: string;
+  completed: boolean;
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'shopping-list';
+
+  items: Item[] = [];
+  newItemName: string = '';
+
+  addItem() {
+    const newItem: Item = {name: this.newItemName, completed: false};
+    this.items.push(newItem);
+    this.newItemName = '';
+  }
+
+  completeItem(item: Item) {
+    item.completed = true;
+  }
+
+  deleteItem(item: Item) {
+    const index = this.items.indexOf(item);
+    if (index >= 0) {
+      this.items.splice(index, 1);
+    }
+  }
+
 }
